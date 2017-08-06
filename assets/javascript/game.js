6c0f43f8bdd81bfdabcb4e1ca8476ec287844f49
Achea Compton <acheacompton@Acheas-MacBook-Pro.local>
2017-08-05 23:45:43 -0700

    final

---
+++ assets/javascript/game.js
@@ -0,0 +1,41 @@
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guessesThusFar = [];
+var computerChoices = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+document.onkeyup = function(event) {
+
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase(); 
+
+    var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+    guessesThusFar.push(userGuess); 
+
+        if (userGuess === computerGuess) {
+            alert("You Win! :)");
+            wins++; 
+            guessesLeft = 10;
+            guessesThusFar.length = 0;
+        }
+
+        else if (guessesLeft === 0) {
+            alert("You Lose :(");
+            losses++;
+            guessesLeft = 10;
+            guessesThusFar.length = 0;
+        }
+
+        else if (userGuess !== computerGuess) {
+            guessesLeft--;
+         }
+
+    var html = 
+    "<h1>Can you guess the letter of which I'm thinking?</h1>" +
+    "<p>Wins: " + wins + "</p>" +
+    "<p>Losses: " + losses + "</p>" +
+    "<p>Guesses Left: " + guessesLeft + "</p>" +
+    "<p>Your guesses, thus far: " + guessesThusFar + "</p>";
+
+    document.getElementById("game").innerHTML = html;
+}
